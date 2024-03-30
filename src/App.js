559c8dedf2873ae004497508ff22889f1b1ed772
Puneet Sharma <puneetsharma@Puneets-MacBook-Air.local>
2024-03-31 01:48:22 +0530

    pusing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import "./App.css";
+// Importing the components we will use in our
+const handleNoHover = () => {
+  var x = document.getElementById('no-btn');
+  x.style.position = "absolute";
+  x.style.top = Math.random() * 90 + "vh"; 
+  x.style.left = Math.random() * 90 + "vh"; 
 }
+const handleYes = () => {
+  document.getElementsByTagName('h1')[0].textContent = "Congrats you are mine 😘😘❤️";
+  console.log("yes");
+}
+const App = () => {
+  return (
+    <>
+    <div className="cutepie">
+      <img src="https://media2.giphy.com/media/yc2pHdAoxVOrJ2m5Ha/200.webp?cid=ecf05e4753ezatati6uik18eqrznjmofteht13l1f30qazop&ep=v1_gifs_related&rid=200.webp&ct=g" />
+      <div>
+        <h1>  Do you like me ?</h1>
+        <button onClick = {handleYes}>yes</button>
+        <button id= "no-btn" onMouseEnter = {handleNoHover}>No</button>
+      </div>
+    </div>
+    </>
+  );
+};
 
 export default App;
